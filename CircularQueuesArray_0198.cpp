ed8c8293c1acf2ec2b69c0071e60aa7f92015c79
Muflay <muflihattamirmuslim@gmail.com>
2026-05-29 16:41:23 +0700

    CCQA -             // jika FRONT <= REAR, iterasi dari FRONT hingga akhir array

--- CircularQueuesArray_0198.cpp
+++ CircularQueuesArray_0198.cpp
@@ -91,12 +91,22 @@ public:
         {
         while (FRONT_Position <= REAR_Position)
         {
-        cout << queue_array[FRONT_Position] << " ";
-        FRONT_Position++;
+            cout << queue_array[FRONT_Position] << " ";
+            FRONT_Position++;
         }
         cout << endl;
         }
         else
         {
+            // jika FRONT <= REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
+        }
     }
 }
